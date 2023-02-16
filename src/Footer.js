10050f2c32dedd5f1c9165eb8c6250a05ffb4cf9
orlinskia <artorl95@gmail.com>
2023-02-16 23:59:37 +0100

    Footer + date change

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+let Footer = () => {
+    return(
+        <div className="footer-parent">
+            <div className="footer">
+                <p>Copyright © 2023 Zespół Szkół Informatycznych</p>
+                <p>Created by: Artur Orliński, Klaudia Strzeszkowska, Mikołaj Wojtyś</p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
